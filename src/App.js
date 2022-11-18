6805f6eb40d1d5907185ed43e21050a0613a658a
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-11-18 17:07:21 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [endereco, setEndereco] = useState({});
+
+  const [enderecos, setEnderecos] = useState([]);
+
+  function manipularEndereco(evento) {
+    const cep = evento.target.value;
+    setEndereco({ cep });
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+          setEnderecos((lista) => [...lista, endereco]);
+
+          setEndereco((enderecoAntigo) => {
+            return {
+              ...enderecoAntigo,
+              cep: cep,
+              rua: dados.logradouro,
+              bairro: dados.bairro,
+              cidade: dados.cidade,
+              estado: dados.uf,
+            }
+          });
+
+          /*
+          setEndereco({
+            cep: cep,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.cidade,
+            estado: dados.uf,
+          });
+          */
+        });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Hooks - useState</h1>
+        <input
+          type="number"
+          placeholder="Digite o cep"
+          onChange={manipularEndereco}
+        />
+        <div>
+          <h2>Endereço</h2>
+          <ul>
+            <li>CEP: {endereco.cep}</li>
+            <li>Rua: {endereco.rua}</li>
+            <li>Bairro: {endereco.bairro}</li>
+            <li>Cidade: {endereco.cidade}</li>
+            <li>Estado: {endereco.estado}</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Endereços</h2>
+          <ul>
+            {enderecos.map((endereco, index) => {
+              return (
+                <li key={index}>{endereco}</li>
+              );
+            })}
+          </ul>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+/*
+import { useState } from "react";
+import './App.css';
+
+function App() {
+  const [cep, setCep] = useState("");
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input
+          type="number"
+          placeholder="Digite o cep"
+          onChange={(evento) => setCep(evento.target.value)}
+        />
+        <ul>
+          <li>CEP: {cep}</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
